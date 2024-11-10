e8bd611853f5f004cb9f4226b2cb62a6b4c0cfaf
lucasichen <lucas@ichen.ca>
2024-11-10 16:26:53 -0500

    feat: add password reqs

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -346,12 +346,17 @@ function Dashboard() {
                     name="password"
                     label="Password"
                     rules={[
-                      { required: true, message: 'Please enter the password' }
+                      { required: true, message: 'Please enter the password' },
+                      { min: 8, message: 'Password must be at least 8 characters' },
+                      { pattern: /[A-Z]/, message: 'Password must contain at least one uppercase letter' },
+                      { pattern: /[a-z]/, message: 'Password must contain at least one lowercase letter' },
+                      { pattern: /\d/, message: 'Password must contain at least one number' },
+                      { pattern: /[!@#$%^&*]/, message: 'Password must contain at least one special character (!@#$%^&*)' },
                     ]}
                   >
                     <AntInput.Password />
                   </Form.Item>
-                  <Form.Item
+                  {/* <Form.Item
                     name="website"
                     label="Website"
                     rules={[
@@ -359,7 +364,7 @@ function Dashboard() {
                     ]}
                   >
                     <AntInput />
-                  </Form.Item>
+                  </Form.Item> */}
                 </Form>
               </Modal>
             </div>
