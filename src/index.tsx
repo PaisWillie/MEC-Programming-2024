27e1f75d8108bbccdd1d8f9b6f09d723d8eeb49e
Willie Pai <PaisWillie@gmail.com>
2024-11-10 09:48:40 -0500

    feat: add react router

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,16 @@ import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
 import App from 'components/App'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
-root.render(<App />)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  }
+])
+
+root.render(<RouterProvider router={router} />)
